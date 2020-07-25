df68e06bde8c1a3e12f1f92af1c0d9148b39d06a
Athanasios Kostopoulos <athanasios@akostopoulos.com>
2020-07-25 19:07:10 +0200

    removed redudant shell command requirements and introduced command line argument for logfile

--- associator.py
+++ associator.py
@@ -5,37 +5,42 @@ import subprocess
 import shutil
 import tempfile
 import os
+import argparse
+
 def has_tool(name):
     """Check whether `name` is on PATH and marked as executable."""
     return shutil.which(name) is not None
 
-req_tools = [ "kubectl", "awk"]
+parser = argparse.ArgumentParser()
+parser.add_argument("-l","--logfile", help="the logfile we want to transform", default="vault-1")
+args = parser.parse_args()
+# in case we need more tools from the OS
+req_tools = [ "kubectl"]
 for tool in req_tools:
     if not has_tool(tool):
         print("%s not found - exiting", tool)
         sys.exit(1)
 pod_ips = tempfile.mkstemp(text=True)[1]
 kubelet_cmd = ["kubectl", "get",  "pods", "-o", "wide", "--all-namespaces"]
-awk_cmd = ["awk", "\\'{", "print", "$2", " ",  "$7", "}\\'"] # + pod_ips
-tee_cmd = ["tee", pod_ips]
+# the shell way to do this is but we are not going to need it
 # kubectl get pods -o wide --all-namespaces |gawk '{ print $2 " "  $7 }' |tee pod_ips
 kubectl = subprocess.Popen(kubelet_cmd, stdout=subprocess.PIPE)
-#awk = subprocess.Popen(awk_cmd, stdin=kubectl.stdout, stdout=subprocess.PIPE)
 pods = kubectl.stdout.readlines()
 f = open(pod_ips, "wb")
 for pod in pods:
     pods = pod.split()
-    f.write(pods[1]+ " ".encode("ascii") + pods[6]+str.encode("\n"))
+    f.write(pods[1]+ " ".encode("ascii") + pods[6]+"\n".encode("ascii"))
 f.close()
 with open(pod_ips, "r") as ifile:
     pods = ifile.readlines()
+# we do not need it anymore ...
 os.unlink(pod_ips)
 helper = {}
 for pod in pods:
     foo = pod.strip().split()
     helper[foo[1]] = foo[0]
 
-with open("vault-1","r") as logfile:
+with open(args.logfile,"r") as logfile:
     logs = logfile.readlines()
 
 rexp_ip = r".*\s(\d{1,3}.\d{1,3}\.\d{1,3}\.\d{1,3})"
